ec812700b20982e3cb2585315e008f1c1dc2ca19
vinay1945 <vinaykumarporandla19@gmail.com>
2024-01-12 17:36:46 +0530

    complete the weather app project

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const APIKey = `9638019b0ce4cd919514be167835c1a8`;
-// let cityName = document.getElementById("Search").value;
+// const APIKey = `9638019b0ce4cd919514be167835c1a8`;
 const Search_btn = document.getElementById("Search_btn");
-let weatherDisplay = document.querySelector(".weather_info");
-// console.log(cityName);
 
 async function weatherApp() {
   let cityName = document.getElementById("Search").value;
-  console.log(cityName);
   let resp = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=9638019b0ce4cd919514be167835c1a8&q=${cityName}`
   );
+
+  if (resp.status === 404) {
+    document.getElementById("error").style.display = "block";
+  }
+
   let data = await resp.json();
-  console.log(data);
-  document.getElementById("temp").innerHTML = `${data.main.temp}°C`;
+  document.getElementById("temp").innerHTML = `${Math.round(data.main.temp)}°C`;
   document.getElementById("cityname").innerHTML = cityName;
   document.getElementById("humidity").innerHTML = `${data.main.humidity}%`;
   document.getElementById("Wind_speed").innerHTML = `${data.wind.speed} Km\h`;
+
+  let imgChange = document.querySelector(".weather_types_img");
+
+  if (data.weather[0].main === "Clouds") {
+    imgChange.src = `/images/clouds.png`;
+  } else if (data.weather[0].main === "Clear") {
+    imgChange.src = `/images/clear.png`;
+  } else if (data.weather[0].main === "Rain") {
+    imgChange.src = `/images/rain.png`;
+  } else if (data.weather[0].main === "Drizzle") {
+    imgChange.src = `/images/drizzle.png`;
+  } else if (data.weather[0].main === "Mist") {
+    imgChange.src = `/images/mist.png`;
+  }
+
+  document.querySelector("#result").style.display = "block";
 }
 
 Search_btn.addEventListener("click", () => {
-  weatherDisplay.computedStyleMap.display = "block";
   weatherApp();
-  //   console.log(cityName);
 });
